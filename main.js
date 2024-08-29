f8392c4e93da8fe245076fa3751e285966a095c6
Mateogit32 <36247420+mateo3264@users.noreply.github.com>
2024-08-29 13:07:43 -0500

    added bars. get data from db

--- main.js
+++ main.js
@@ -2,21 +2,34 @@
 function createChart(dataList){
     const ctx = document.getElementById('myChart').getContext('2d');
     const myChart = new Chart(ctx, {
-        type: 'line',
+        type: 'bar',
         data: {
             labels: ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'],
             datasets: [{
-                label: 'Sales',
+                label: 'Days without smoking',
                 data: dataList,
+                backgroundColor:[
+                    'rgba(200, 200, 0, 0.6)',
+                    'rgba(200, 200, 0, 0.6)',
+                    'rgba(200, 200, 0, 0.6)',
+                    'rgba(200, 200, 0, 0.6)',
+                    'rgba(200, 200, 0, 0.6)',
+                ],
                 borderColor: 'rgb(75, 192, 192)',
                 tension: 0.1
             }]
         },
+        
         options: {
             responsive: true,
             scales: {
                 y: {
-                    beginAtZero: true
+                    beginAtZero: true,
+                    ticks:{
+                        stepSize:1
+                    }
+
+
                 }
             }
         }
@@ -29,9 +42,19 @@ fetch('https://mateoedutec.pythonanywhere.com/')
 .then(response => response.json())
 .then(data => {
     console.log(data);
-    const list = data.data;
+    let running_total = 0;
+    const list = data.map((item)=>
+        {
+            const new_item = item.data ? 1 : 0;
+            running_total += new_item;
+            return running_total;
+        });
+    console.log('list');
+    console.log(list);
+    const h1Text = document.getElementById('title');
+    h1Text.innerHTML = data[data.length - 1].name;
     const pText = document.getElementById('text');
-    pText.innerHTML = data.name + ' ' + data.age;
+    pText.innerHTML = data.name;
     createChart(list);
 })
 .catch(error => console.error(error))
