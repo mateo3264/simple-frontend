077bc32018967a0fbe71841ae7f2279ddb65e17d
Mateogit32 <36247420+mateo3264@users.noreply.github.com>
2024-09-14 11:26:41 -0500

    changed location of anchor tag. removed hardcoded li elements. added checkbox to each task

--- scripts/add_task.js
+++ scripts/add_task.js
@@ -2,7 +2,10 @@ function add_task(){
     const userInput = document.getElementById("userInput");
     const ul = document.getElementById("unli");
     const li = document.createElement("li");
+    const cb = document.createElement("input");
+    cb.type = "checkbox";
     li.appendChild(document.createTextNode(userInput.value));
+    li.appendChild(cb);
     ul.appendChild(li);
     userInput.value = "";
     userInput.focus();
